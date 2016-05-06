53a641e9b1a4daa298a85efc1ae3934b2f9545b6
Steven Cooper <stevengcooper76@gmail.com>
2016-05-06 08:13:07 -0400

    Adjusted dashboard controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def logged_in?
-   redirect_to login_path, notice: "You must log in to access this page" unless session[:user_id]
- end
+   redirect_to sessions_new_path, notice: "You must log in to access this page" unless session[:user_id]
+  end
 
- private def authenticate_teacher
+  private def authenticate_teacher
    redirect_to :back, notice: "You do not have permission to access that page." unless session[:user_type] == "Teacher"
- end
+  end
 
- private def authenticate_student
+  private def authenticate_student
       redirect_to root_path, notice: "You don't have permission to access that page." unless session[:user_type] == "Student"
   end
 
   private def authenticate_parent
       redirect_to root_path, notice: "You don't have permission to access that page." unless session[:user_type] == "Parent"
   end
-
-
-
 end
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,30 +1,21 @@
 class DashboardsController < ApplicationController
-  before_action :set_dashboard, only: [:show, :edit, :update, :destroy]
-  before_action :move_forward
+  # before_action :move_forward
+
 
   # GET /dashboards
   def index
-    @dashboards = Dashboard.all
+
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_dashboard
-      @dashboard = Dashboard.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def dashboard_params
-      params[:dashboard]
-    end
-
-    def move_forward
-      if session[:user_type] == "Teacher"
-        redirect_to teachers_path
-      elsif session[:user_type] == "Student"
-        redirect_to students_path
-      elsif session[:user_type] == "Parent"
-        redirect_to parents_path
-      end
-    end
+    # def move_forward
+    #   if session[:user_type] == "Teacher"
+    #     redirect_to teachers_path
+    #   elsif session[:user_type] == "Student"
+    #     redirect_to students_path
+    #   elsif session[:user_type] == "Parent"
+    #     redirect_to parents_path
+    #   end
+    # end
 end
